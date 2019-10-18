d708ec4528f7338193703c9152834a6836a47fd2
Aaronczww <785333692@qq.com>
2019-10-18 15:14:20 +0800

    直接光照+IBL

---
+++ Assets/Shader/PBRParaSet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PBRParaSet : MonoBehaviour
+{
+    [SerializeField,Range(0,1)]
+    public float _Smoothness;
+    [SerializeField, Range(0, 1)]
+    public float metallic;
+
+    public Renderer render;
+
+    private MaterialPropertyBlock materialProperty;
+
+    private void Start()
+    {
+        materialProperty = new MaterialPropertyBlock();
+    }
+    private void Update()
+    {
+        materialProperty.SetFloat("_Smoothness", _Smoothness);
+        materialProperty.SetFloat("metallic", metallic);
+        render.SetPropertyBlock(materialProperty);
+    }
+}
+
